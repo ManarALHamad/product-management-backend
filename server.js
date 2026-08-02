3f28470279a8b21b9a5a79ad62aa6a7be14e42f2
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-02 15:58:22 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// server.js
+
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name} 🥭`)
+})
+
+// this line allows our React Front End permission to connect to our backend
+app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to the Product Management API' })
+})
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`The express app is running on port ${port}`)
+})
